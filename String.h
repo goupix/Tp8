47d8362215648651bdc2294710109726c2a2c53a
Alexis Pister <alexis.pister@hotmail.com>
2017-01-16 15:26:10 +0100

    ajout de String.h

---
+++ String.h
@@ -0,0 +1,17 @@
+class String {
+	public :
+		String();
+		String(
+
+		
+
+
+
+	protected :
+		char chaine[];
+		int lenght; //Nombres de caracteres
+		int cap; //Nombres de cases totales du tableau
+
+
+
+};
