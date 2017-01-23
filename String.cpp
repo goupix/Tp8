c2cf1f43f962bdf9a2fee8276015d9163008722f
Meghane Decroocq <meghdecroocq@gmail.com>
2017-01-23 16:28:36 +0100

    constructeur par copie

--- String.cpp
+++ String.cpp
@@ -8,27 +8,42 @@ String::String (){
   cap=10;
   chaine=new char[cap];
   length_=5;
- 
-  
-
+  chaine[5]='\0';
 }
 
-int String::length(){
+
+int String::length() const {
   std::cout<<length_<<std::endl;
 
   return length_ ;     
 
 }
 
-int String::capacity(){
+int String::capacity() const{
 
   return cap;
+
 }
+
+
+char* String:: c_str() const{
+  char* p=chaine;
+  return p;
+
+}
+
 
 String::String( const char* s){
 
   cap=sizeof(s);
 
+
+}
+
+String::String(const String& str){
+  length_=str.length();
+  cap=str.capacity();
+  chaine=str.c_str();
 
 }
 
